af140ea2aa0231d2fe286c8d5326fd80be38f750
BRANDON194 <brandonmichelin194@gmail.com>
2016-04-21 22:33:59 -0300

    dwadwa
    thiasojodsa

---
+++ SuperDuperGame/Assets/Scripts/Buttons/OptionsButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick(){
+		Debug.Log ("Button Clicked");
+	}
+}
