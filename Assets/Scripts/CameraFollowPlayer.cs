ac233308d0242865f4ce5dae7d7565474102e4e3
BRANDON194 <brandonmichelin194@gmail.com>
2016-04-21 23:42:17 -0300

    My First Commit.

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 PlayerPos = transform.position;
+
+		Camera.main.transform.position = new Vector3 (PlayerPos.x, PlayerPos.y, -10);
+	}
+}
